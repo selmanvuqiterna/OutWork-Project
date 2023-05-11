6c154c2026da185e67e843e4d5806e23e1d1bb6b
selmanvuqiterna <selmanvuqitern@gmail.com>
2023-05-11 17:41:58 +0200

    createUser

--- api/app.js
+++ api/app.js
@@ -17,9 +17,40 @@ const db = mysql.createConnection({
 app.use(express.json());
 app.use(cors())
 
+
+//insertimi i nje useri prej backend ne databaz
 app.get("/", (req,res)=>{
+  const sqlInsert = "INSERT INTO users (fullname, email, password, confirmPassword)VALUES ('Filan Fisteku', 'filan@gmail.com','filan123','filan123')";
+
+  db.query(sqlInsert, (err, result)=>{
+    console.log('error', err);
+    console.log('result', result);
     res.json("hello this is backend")
+  })
+    
 })
+
+
+app.post('/create', (req,res) =>{
+  const query = "INSERT INTO users (`fullname`, `email`, `password`, `confirmPassword`)   VALUES(?)";
+
+  const values  = [
+    req.body.fullname,
+    req.body.email,
+    req.body.password,
+    req.body.confirmPassword
+  ]
+
+  db.query(query, [values], (err,data) =>{
+    if(err) return res.json("Error")
+    return res.json(data)
+  })
+})
+
+
+
+  
+
 
 app.get("/employees", (req,res)=>{
     const q = "SELECT * FROM employees"
--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useReducer, useState } from "react";
 import "./register.css";
 import Navbar from "../../components/Navbar/Navbar";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 
 const Register =()=>{
+         const [fullname, setFullname] = useState('');
+         const [email, setEmail] = useState('');
+         const [password, setPassword] = useState('');
+         const [confirmPassword, setConfirmPassword] = useState('');
+         const navigate = useNavigate(); 
+
+         function handleSubmit(event){
+            event.preventDefault();
+            axios.post('http://localhost:8800/create', {fullname,email,password,confirmPassword})
+            .then(res =>{
+                console.log(res);
+                navigate('/');
+            }).catch(err => console.log(err))
+         }
+
+
     return(
         <div className="body-register">
             <div className="navbar">
@@ -18,12 +36,12 @@ const Register =()=>{
                 </div>
 
                 <div className="register-form">
-                    <form action="" className="form">
-                    <input className="inputs-form"  type="text" placeholder="Full Name" />
-                    <input className="inputs-form"  type="text" placeholder="Email" />
-                    <input className="inputs-form" type="password" placeholder="Password" />
-                    <input className="inputs-form" type="password" placeholder="Confirm Password" />
-                    <input className="inputs-button" id="register-btn" type="submit" value={"Register"}  />
+                    <form action="" className="form" method="POST" onSubmit={handleSubmit} >
+                    <input className="inputs-form"  type="text" placeholder="Full Name" name="fullname" onChange={e=> setFullname(e.target.value)} />
+                    <input className="inputs-form"  type="text" placeholder="Email" name="email" onChange={e=> setEmail(e.target.value)} />
+                    <input className="inputs-form" type="password" placeholder="Password" name="password" onChange={e=> setPassword(e.target.value)} />
+                    <input className="inputs-form" type="password" placeholder="Confirm Password" name="confirmPassword" onChange={e=> setConfirmPassword(e.target.value)} />
+                    <button className="inputs-button" id="register-btn" type="submit" value={"Register"} > Register</button>
 
                     </form>
                     <div  id="already"><a href="/Login">Already have an account?</a></div>
