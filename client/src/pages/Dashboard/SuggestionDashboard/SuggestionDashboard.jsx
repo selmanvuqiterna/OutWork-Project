cc7adce3f35b78ee9d19317406a3215a41f4b20f
Esned <97762577+Esned@users.noreply.github.com>
2023-05-19 22:42:06 +0200

    Update SuggestionDashboard.jsx
    creating suggestion dashboard page and component

---
+++ client/src/pages/Dashboard/SuggestionDashboard/SuggestionDashboard.jsx
@@ -0,0 +1,18 @@
+import React from "react-routr-dom";
+import "./SuggestionD.css";
+
+
+const SuggestionDashboard =()=>{
+
+  
+  return(
+    
+  <h1>Hello from suggstion </h1>
+  
+  )
+
+}
+
+
+
+export default SuggestionDashboard;
