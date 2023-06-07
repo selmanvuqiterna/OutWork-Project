29075fda70ee22bb09a56a6f012adb866fb149f3
besar77 <bd48380@ubt-uni.net>
2023-06-07 21:07:42 +0200

    Rregullime ne faqen aplikime

--- client/src/pages/Aplikimet/Aplikimet.jsx
+++ client/src/pages/Aplikimet/Aplikimet.jsx
@@ -133,6 +133,37 @@ const Aplikimet = () => {
             <p className="aplikimi-lloji">Full Time</p>
           </div>
         </div>
+
+        <div className="puna-aplikimi">
+          <div className="aplikimi-titulli">
+            <p className="titulli-aplikimi">
+              Praktikante ne Inxhinieri softuerike
+            </p>
+            <p className="emri-kompanis-aplikimi">
+              <FontAwesomeIcon icon={faUser} style={{ marginRight: "7px" }} />
+              PR CONSULTING SH.P.K
+            </p>
+            <p className="shteti-aplikimi">
+              <FontAwesomeIcon
+                icon={faMapMarkerAlt}
+                style={{ marginRight: "7px" }}
+              />
+              Kosovë
+            </p>
+          </div>
+
+          <div className="aplikimi-largo">
+            <button className="aplikimi-fshije">Largo</button>
+            <p className="aplikimi-data">
+              <FontAwesomeIcon
+                icon={faCalendarAlt}
+                style={{ marginRight: "7px" }}
+              />
+              Apr 06, 2023
+            </p>
+            <p className="aplikimi-lloji">Full Time</p>
+          </div>
+        </div>
       </div>
     </div>
   );
